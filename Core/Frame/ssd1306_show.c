20b87044eed2b749ccac4880f946697b72391603
Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
2024-12-20 19:36:28 +0800

    update ssd1306_show function

--- Core/Frame/ssd1306_show.c
+++ Core/Frame/ssd1306_show.c
@@ -10,7 +10,7 @@
 #ifdef SSD1306_OLED
 
 /* 128x64 OLED with 7x10 FONT = 18x6 */
-// init null page
+// init page data
 char page_data[BUFF_SIZE_COLS][BUFF_SIZE_ROWS] = {
         "                 ",
         "                 ",
@@ -21,36 +21,24 @@ char page_data[BUFF_SIZE_COLS][BUFF_SIZE_ROWS] = {
 };
 
 void ssd1306_show(void) {
-
-        int y = 0;
-
-        ssd1306_Fill(Black);
-        ssd1306_SetCursor(2, y);
-        ssd1306_WriteString(&page_data[0][0], Font_data, White);
-        y += Y_OFFSET;
-
-        ssd1306_SetCursor(2, y);
-        ssd1306_WriteString(&page_data[1][0], Font_data, White);
-        y += Y_OFFSET;
-
-        ssd1306_SetCursor(2, y);
-        ssd1306_WriteString(&page_data[2][0], Font_data, White);
-        y += Y_OFFSET;
+    ssd1306_Fill(Black);
 
 #if defined(SSD1306_INCLUDE_FONT_7x10)
-        ssd1306_SetCursor(2, y);
-        ssd1306_WriteString(&page_data[3][0], Font_data, White);
-        y += Y_OFFSET;
-
-        ssd1306_SetCursor(2, y);
-        ssd1306_WriteString(&page_data[4][0], Font_data, White);
-        y += Y_OFFSET;
-
-        ssd1306_SetCursor(2, y);
-        ssd1306_WriteString(&page_data[5][0], Font_data, White);
+    const int num_lines = 6;
+#else /* SSD1306_INCLUDE_FONT_11x18 */
+    const int num_lines = 3;
 #endif
-        ssd1306_UpdateScreen();
+
+    int y = 0;
+    for (int i = 0; i < num_lines; i++) {
+        ssd1306_SetCursor(2, y);
+        ssd1306_WriteString(&page_data[i][0], Font_data, White);
+        y += Y_OFFSET;
+    }
+
+    ssd1306_UpdateScreen();
 }
+
 #else
 void ssd1306_show(void) {return;}
 #endif /* SSD1306_OLED */
